9e9ddb446d653dab88c919e2d2c72956d984e742
Maria Rykova <maria.nigay@yandex.ru>
2021-02-20 21:12:09 +0500

    main

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function() {
       evt.preventDefault();
       http.open("POST", "contact.php", true);
       http.onreadystatechange = function() {
-        if (http.readyState == 3 && http.status == 200) {
+        if (http.readyState == 4 && http.status == 200) {
           alert(http.responseText);
           if (http.responseText.indexOf(f.nameFF.value) == 0) { // очистить поля формы, если в ответе первым словом будет имя отправителя (nameFF)
             th.trigger("reset");
@@ -22,61 +22,81 @@ document.addEventListener('DOMContentLoaded', function() {
    
   });
 
-//document.addEventListener('DOMContentLoaded', function(){
-//    let form = document.getElementById('form');
-//   let submit = document.getElementById('submit');
- //   submit.addEventListener('submit', formSend);
+// document.addEventListener('DOMContentLoaded', function(){
+//     let form = document.getElementById('form');
+//     let submit = document.getElementById('submit');
+//     submit.addEventListener('submit', formSend);
 
- //   async function formSend(e) {
- //       e.preventDefault();
-//
-  //      let error = formValidate(form);
-//
-  //      console.log('pw');
-    //}
+//     async function formSend(e) {
+//         e.preventDefault();
 
-  //  function formValidate(form){
-    //    let error = 0;
-    //    let formReq = document.querySelectorAll('._req');
+//         let error = formValidate(form);
 
-      //  for (let index = 0; index < formReq.length; index++) {
-        //    const input = formReq[index];
+//         let formData = new FormData(form);
 
-        //    if(input.classList.contains('_email')){
-         //       if (emailTest(input)) {
-         //           formAddError(input);
-         //           error++;
-         //           console.log("ошибка")
-         //       }
-         //   } else {
-        //        if(input.value === ''){
-        //            formAddError(input);
-         //           console.log('hi');
-         //           error++;
-         //       }
-         //   }
+//         if(error === 0) {
+//             //form.classList.add('._sending');
+//             let response = await fetch('sendmail.php', {
+//                 method: 'POST',
+//                 body: formData
+//             });
+
+//             if (response.ok) {
+//                 let result =  await response.json();
+//                 alert(result.message);
+//                 form.reset();
+//             } else {
+//                 alert('Ошибка');
+//             }
+//         } else {
+//             alert('Заполните обязательные поля');
+//         }
+
+//         console.log('pw');
+//     }
+
+//     function formValidate(form){
+//         let error = 0;
+//         let formReq = document.querySelectorAll('._req');
+
+//         for (let index = 0; index < formReq.length; index++) {
+//             const input = formReq[index];
+
+//             if(input.classList.contains('_email')){
+//                 if (emailTest(input)) {
+//                     formAddError(input);
+//                     error++;
+//                     console.log("ошибка")
+//                 }
+//             } else {
+//                 if(input.value === ''){
+//                     formAddError(input);
+//                     console.log('hi');
+//                     error++;
+//                 }
+//             }
 
 
             
-     //   }
+//         }
 
-     //   console.log(hello);
-   // }
+//         return error;
+//     }
 
-  //  function formAddError(input) {
-     //   input.parentElement.classList.add('_error');
-   //     input.classList.add('_error');
-   //     console.log("ошибка")
-  //  }
-//
-  //  function formRemoveError(input) {
-    //    input.parentElement.classList.remove('_error');
-    //    input.classList.remove('_error');
-    //    console.log("ошибка")
- //   }
+//     function formAddError(input) {
+//         input.parentElement.classList.add('_error');
+//         input.classList.add('_error');
+//         console.log("ошибка")
+//     }
 
- //   function emailTest(input) {
-  //      return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.w{2,8})+$/.test(input.value);
- //   }
+//     function formRemoveError(input) {
+//         input.parentElement.classList.remove('_error');
+//         input.classList.remove('_error');
+//         console.log("ошибка")
+//     }
 
-//});
+//     function emailTest(input) {
+//         return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.w{2,8})+$/.test(input.value);
+//     }
+
+// });
